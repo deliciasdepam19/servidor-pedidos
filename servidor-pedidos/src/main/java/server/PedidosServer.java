a55becff1668823c6aa9be5c8a08f06b4b566fde
deliciasdepam19 <deliciasdepam19@gmail.com>
2026-05-06 22:18:05 -0400

    Update PedidosServer.java

--- servidor-pedidos/src/main/java/server/PedidosServer.java
+++ servidor-pedidos/src/main/java/server/PedidosServer.java
@@ -113,7 +113,7 @@ public class PedidosServer {
                         e.printStackTrace();
                         enviarRespuesta(exchange, 400, "{\"exito\":false}");
                     }
-                } /
+                } 
             }
         });
 
